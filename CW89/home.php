2c3720b076c4f3a322274e9ea3b80d1fdff6373c
felipeflourwears <A01274880@itesm.mx>
2022-10-07 03:01:18 -0500

    Ready to Injection

--- CW89/home.php
+++ CW89/home.php
@@ -1,3 +1,12 @@
-<?php
-echo "Hello world";
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+<h1>BIENVENIDOS</h1>
+    
+</body>
+</html>
--- CW89/index.php
+++ CW89/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale-1.0">
     <title>login</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/cabecera.css">
 </head>
 <body>
     <form action="validar.php" method="post">
